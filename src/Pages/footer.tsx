b3efcd538667e64a6dcdc65705b1bc715cb39f8f
prakash <pinnub23@gmail.com>
2023-06-26 17:46:12 +0530

    navbar + contact + footer

--- src/Pages/footer.tsx
+++ src/Pages/footer.tsx
@@ -10,7 +10,7 @@ export default function Footer() {
     <>
       <div className="flex flex-col items-center my-16 gap-[25px] justify-center h-[165px]">
         <div>
-          <h1 className="font-nexa text-5xl font-extrabold leading-20 tracking-normal text-center text-primary-dark">
+          <h1 className="font-nexa text-[30px] md:text-5xl font-extrabold leading-20 tracking-normal text-center text-primary-dark">
             Ready to get Started?
           </h1>
         </div>
@@ -20,7 +20,7 @@ export default function Footer() {
             className=" bg-primary-dark flex gap-2 items-center focus:ring-4 focus:ring-green-300 border  font-medium rounded-full text-sm px-4 py-2 text-center"
           >
             <img src={phone} className="h-5 w-5" alt="" />
-            <span className="font-inter text-base font-medium leading-7 tracking-tight text-white text-center">
+            <span className="font-inter text-[12px] md:text-base md:font-medium leading-7 tracking-tight text-white text-center">
               Let’s get on a call
             </span>
           </button>
@@ -38,7 +38,7 @@ export default function Footer() {
             <div className="flex items-center">
               <a href="#" className="flex items-center gap-5">
                 <div className="bg-primary-green h-[7px] w-[14px]"></div>
-                <img src={logo} className="w-[206px] h-[22px]" alt="" />
+                <img src={logo} className="h:[20px] w-[135px] md:w-[206px] md:h-[22px]" alt="" />
                 <div className="bg-primary-green h-[7px] w-[14px]"></div>
                 <div></div>
               </a>
